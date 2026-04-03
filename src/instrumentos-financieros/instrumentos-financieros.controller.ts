606732afabfe1c87098acdcc0da821b84ddcf62c
Tiziano Tomas Luzi Ramos <tizianoluziramos@gmail.com>
2026-04-03 18:32:07 -0300

    feature

--- src/instrumentos-financieros/instrumentos-financieros.controller.ts
+++ src/instrumentos-financieros/instrumentos-financieros.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { InstrumentosFinancierosService } from './instrumentos-financieros.service';
-import { ResponseDTO } from './dto/response-instrumento-financiero.dto';
+import { ResponseDTO } from './dto/response';
 import { ApiBearerAuth, ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateInstrumentoFinancieroDto } from './dto/create-instrumento-financiero.dto';
 import { UpdateInstrumentoFinancieroDto } from './dto/update-instrumento-financiero.dto';
