17aa333f577828e499e9a0a3293fab62e615265b
Tiziano Tomas Luzi Ramos <tizianoluziramos@gmail.com>
2026-04-03 17:23:57 -0300

    Fixed the entire problem

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }));
   const options = new DocumentBuilder().setTitle('Liberty Finance').setDescription('Tu camino a la libertad financiera.').setVersion('1.0').addTag('Endpoints').addBearerAuth().build();
 
   const document = SwaggerModule.createDocument(app, options);
