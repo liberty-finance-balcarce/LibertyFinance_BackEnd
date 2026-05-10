e0b843a2fa7ef2f0661f62eda242e598123ee12e
Matías Méndez <matiasmendez722@gmail.com>
2026-05-10 16:36:55 -0300

    Creacion de Seeder Rol

--- src/rol/rol.module.ts
+++ src/rol/rol.module.ts
@@ -1,8 +1,8 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { Rol } from "./entities/rol.entity";
-import { RolController } from "./rol.controller";
-import { RolService } from "./rol.service";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Rol } from './entities/rol.entity';
+import { RolController } from './rol.controller';
+import { RolService } from './rol.service';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Rol])],
