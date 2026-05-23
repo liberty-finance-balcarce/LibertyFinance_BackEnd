a6fde0315d9bbb111b86cdc04e0ff0371e20df00
Matías Méndez <matiasmendez722@gmail.com>
2026-05-23 18:41:41 -0300

    .

--- src/database/seed.ts
+++ src/database/seed.ts
@@ -1,8 +1,19 @@
 import { AppDataSource } from './data-source';
+import { seedInstrumentosFinancieros } from './seeders/instrumentos-financieros';
+import {seedPerfilInversor} from './seeders/perfil-inversor';
+import {seedRols} from './seeders/rol'
 
 async function runSeeders(): Promise<void> {
   await AppDataSource.initialize();
+
   console.info('Database connected. Running seeders...\n');
+
+  console.log('Seeding Instrumentos Financieros...');
+  await seedInstrumentosFinancieros(AppDataSource);
+
+  console.log('Seeding Perfil Inversor...');
+  await seedPerfilInversor(AppDataSource);
+
   await AppDataSource.destroy();
   console.log('\nAll seeders completed successfully.');
 }
