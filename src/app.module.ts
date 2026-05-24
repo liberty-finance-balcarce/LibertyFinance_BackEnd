c14c7fd616e1589828ee18d83e8296acc1bc2a9a
Tiziano Tomas Luzi Ramos <tizianoluziramos@gmail.com>
2026-05-24 13:54:19 -0300

    Terminado

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { InstrumentosFinancierosModule } from './instrumentos-financieros/instrumentos-financieros.module';
 import { ProvinciasModule } from './provincias/provincias.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { TransaccionHistoricoVentaModule } from './transaccion-historico-venta/transaccion-historico-venta.module';
 import { TransaccionHistoricoCompraModule } from './transaccion-historico-compra/transaccion-historico-compra.module';
+import { RolModule } from './rol/rol.module';
+import { AuthModule } from './auth/auth.module';
+import { PerfilInversorModule } from './perfil-inversor/perfil-inversor.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }),
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
@@ -21,13 +25,16 @@ import { TransaccionHistoricoCompraModule } from './transaccion-historico-compra
       entities: [__dirname + '/**/*.entity.{js,ts}'],
       synchronize: false,
     }),
+    AuthModule,
     InstrumentosFinancierosModule,
+    PerfilInversorModule,
     ProvinciasModule,
-    UsuariosModule,
+    RolModule,
     TransaccionHistoricoCompraModule,
-    TransaccionHistoricoVentaModule
+    TransaccionHistoricoVentaModule,
+    UsuariosModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
