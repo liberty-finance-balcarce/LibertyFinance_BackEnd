d8e5ca969309adba8b43569cae1cd6b784e61256
Tiziano Tomas Luzi Ramos <tizianoluziramos@gmail.com>
2026-06-09 11:10:47 -0300

    Arreglado loco!!

--- src/transaccion-historico-compra/entities/transaccion-historico-compra.entity.ts
+++ src/transaccion-historico-compra/entities/transaccion-historico-compra.entity.ts
@@ -1,34 +1,35 @@
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { InstrumentoFinanciero } from '../../instrumentos-financieros/entities/instrumento-financiero.entity';
 import { Usuario } from '../../usuarios/entities/usuario.entity';
 
-@Entity ('transaccion_historico_compra')
-
+@Entity('transaccion_historico_compra')
 export class TransaccionHistoricoCompra {
-    @PrimaryGeneratedColumn('increment')
-    id_transaccion_compra: number;
+  @PrimaryGeneratedColumn('increment')
+  id_transaccion_compra: number;
 
-    @Column({ type: 'date' })
-    fecha_operacion: Date;
+  @Column({ type: 'date' })
+  fecha_operacion: Date;
 
-    @ManyToOne(() => InstrumentoFinanciero)
-    @JoinColumn({ 
-        name: 'id_instrumento',
-        referencedColumnName: 'id_instrumento'    
-    })
-        id_instrumento: InstrumentoFinanciero;
+  @ManyToOne(() => InstrumentoFinanciero)
+  @JoinColumn({
+    name: 'id_instrumento',
+    referencedColumnName: 'id_instrumento',
+  })
+  id_instrumento: InstrumentoFinanciero;
 
-    @ManyToOne(()=> InstrumentoFinanciero)
-    @JoinColumn({ 
-        name: 'precio_instrumento',
-        referencedColumnName: 'precio_instrumento'
-    })
-    precio_instrumento: number;
+  @Column({ type: 'float', nullable: false })
+  precio_instrumento: number;
 
-    @ManyToOne(()=> Usuario)
-    @JoinColumn({ 
-        name: 'dni_usuario',
-        referencedColumnName: 'dni_usuario'
-    })
-    dni_usuario: Usuario;
+  @ManyToOne(() => Usuario)
+  @JoinColumn({
+    name: 'dni_usuario',
+    referencedColumnName: 'dni_usuario',
+  })
+  dni_usuario: Usuario;
 }
