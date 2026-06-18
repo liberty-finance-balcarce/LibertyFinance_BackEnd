4c55560d0071614be447d8abcfeb35e86357c4c7
milena-martinez04 <mariamilenamartinez04@gmail.com>
2026-06-18 20:37:47 -0300

    Se agrego softDelete en usuario

--- src/usuarios/entities/usuario.entity.ts
+++ src/usuarios/entities/usuario.entity.ts
@@ -1,7 +1,7 @@
 import { PerfilInversor } from 'src/perfil-inversor/entities/perfil-inversor.entity';
 import { Provincia } from 'src/provincias/entities/provincia.entity';
 import { Rol } from 'src/rol/entities/rol.entity';
-import { Column, DeleteDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn, UpdateDateColumn } from 'typeorm';
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity('usuarios')
 export class Usuario {
@@ -32,8 +32,11 @@ export class Usuario {
   @Column()
   direccion: string;
 
-  @UpdateDateColumn({name: 'update_at'})
-  update_at: Date;
+  @CreateDateColumn({name: 'created_at'})
+  created_at: Date;
+
+  @UpdateDateColumn({name: 'updated_at'})
+  updated_at: Date;
 
   @DeleteDateColumn({name: 'deleted_at', nullable: true, select: false})
   daleted_at: Date;
