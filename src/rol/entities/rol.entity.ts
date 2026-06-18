b04b97f66ab0acb53c75503892b83dd3b5ac6c98
Matías Méndez <matiasmendez722@gmail.com>
2026-06-18 20:54:43 -0300

    Implementacion de Soft Delete en Rol

--- src/rol/entities/rol.entity.ts
+++ src/rol/entities/rol.entity.ts
@@ -1,17 +1,26 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, CreateDateColumn, UpdateDateColumn, DeleteDateColumn } from 'typeorm';
 import { Usuario } from '../../usuarios/entities/usuario.entity';
 
 @Entity('rol')
 export class Rol {
-    @PrimaryGeneratedColumn('increment')
-    id_rol: number;
+  @PrimaryGeneratedColumn('increment')
+  id_rol: number;
 
-    @Column({
-        type: 'varchar',
-        unique: true,
-    })
-    nombre: string;
+  @Column({
+    type: 'varchar',
+    unique: true,
+  })
+  nombre: string;
 
-    @OneToMany(() => Usuario, (usuario) => usuario.rol)
-    usuarios: Usuario[];
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at', nullable: true, select: false })
+  deletedAt: Date;
+
+  @OneToMany(() => Usuario, (usuario) => usuario.rol)
+  usuarios: Usuario[];
 }
