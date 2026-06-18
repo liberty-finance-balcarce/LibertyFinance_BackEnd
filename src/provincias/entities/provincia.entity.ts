c2eeb2600841dbeb4b21b23d9f719789a8df62aa
Matías Méndez <matiasmendez722@gmail.com>
2026-06-18 20:38:05 -0300

    .

--- src/provincias/entities/provincia.entity.ts
+++ src/provincias/entities/provincia.entity.ts
@@ -1,22 +1,9 @@
-import {
-  Column,
-  DeleteDateColumn,
-  Entity,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-} from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('provincias')
 export class Provincia {
   @PrimaryGeneratedColumn()
   id: number;
-
   @Column()
   provincia: string;
-
-  @UpdateDateColumn({ name: 'updated_at' })
-  updatedAt: Date;
-
-  @DeleteDateColumn({ name: 'deleted_at', nullable: true, select: false })
-  deletedAt: Date;
 }
